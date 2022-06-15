82d058b61149f9f3d99ad94f1b707e4d49359a79
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-15 08:39:08 +0700

    menambah component add product

---
+++ src/pages/addPuduct.jsx
@@ -0,0 +1,11 @@
+
+function AddProduct() {
+    return (
+      <div className="App">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut veritatis quasi reprehenderit voluptate aspernatur, hic ipsam earum unde odio? Animi at nobis enim beatae optio rerum veritatis facilis aliquam expedita.
+      </div>
+    );
+  }
+  
+  export default AddProduct;
+  
