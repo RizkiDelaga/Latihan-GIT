fb3a1d9e940b1d2aabc908ae284f21652cfb1619
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-22 00:36:11 +0700

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@
 function Home() {
   return (
     <div className="App">
-      berhasil
+      berhasil tidak terubah
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut veritatis quasi reprehenderit voluptate aspernatur, hic ipsam earum unde odio? Animi at nobis enim beatae optio rerum veritatis facilis aliquam expedita.
     </div>
   );
