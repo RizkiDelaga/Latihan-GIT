0e1f599900546e43bf1583c04162240ed49c23b7
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-22 00:06:07 +0700

    about hapus lorem

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,6 @@
 function About() {
     return (
       <div className="App">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut veritatis quasi reprehenderit voluptate aspernatur, hic ipsam earum unde odio? Animi at nobis enim beatae optio rerum veritatis facilis aliquam expedita.
       </div>
     );
   }
